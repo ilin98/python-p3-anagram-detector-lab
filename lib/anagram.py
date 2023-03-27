30b8de295409625fcdcba3f6056b396e0d6ca4ea
Isaac <isaac.lin.pnc@gmail.com>
2023-03-27 11:54:20 -0400

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
+
 # your code goes here!
+class Anagram():
+
+    def __init__(self, word):
+        self.word = word
+        self.listed_word = sorted(word)
+
+    def match(self, words):
+        anagram = []
+        for word in words:
+            if self.listed_word == sorted(list(word)):
+                anagram.append(word)
+            else:
+                pass
+        return anagram
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
